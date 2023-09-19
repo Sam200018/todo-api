5bc33485897d7ac7bd6db6af47b0e4c2f5f16621
Samuel Bautista <samuelbaubas@gmail.com>
2023-09-19 14:09:47 -0600

    Todo controller implemented

--- src/main/java/com/example/todoapp/Controller/TodoController.java
+++ src/main/java/com/example/todoapp/Controller/TodoController.java
@@ -1,13 +1,50 @@
 package com.example.todoapp.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.todoapp.Model.Task;
+import com.example.todoapp.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TodoController {
 
+    @Autowired
+    private TodoRepository todoRepository;
+
     @GetMapping("/")
     public String holamundo() {
         return "Hola mundo!!";
+    }
+
+    @GetMapping(value = "/tasks")
+    public List<Task> getTasks() {
+        return todoRepository.findAll();
+    }
+
+    @PostMapping("/saveTask")
+    public String saveTask(@RequestBody Task task) {
+        todoRepository.save(task);
+        return "Saved task";
+    }
+
+    @PutMapping("/updateTask/{id}")
+    public String updateTask(@PathVariable long id, @RequestBody Task taks) {
+        Task updatedTask = todoRepository.findById(id).get();
+        updatedTask.setTitle(taks.getTitle());
+        updatedTask.setDescription(taks.getDescription());
+
+        todoRepository.save(updatedTask);
+
+        return "Updated task";
+
+    }
+
+    @DeleteMapping("/deleteTask/{id}")
+    public String deleteTask(@PathVariable long id) {
+        Task deletedTask = todoRepository.findById(id).get();
+        todoRepository.delete(deletedTask);
+        return "Task deleted";
     }
 }
